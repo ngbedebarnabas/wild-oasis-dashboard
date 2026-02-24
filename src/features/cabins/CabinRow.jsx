87317145a130f9cc667940a2e147fadb95e2bc54
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-02-24 15:48:18 +0100

    React hook form, submitting form data to db and handling errors

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -56,10 +57,12 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      toast.success("Cabin Successfully deleted");
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
+    onError: (err) => toast.error(err.message),
   });
 
   return (
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -13,8 +13,16 @@ const StyleAppLayout = styled.div`
 `;
 const Main = styled.main`
   background-color: var(--color-grey-50);
-
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function AppLayout() {
@@ -24,7 +32,9 @@ function AppLayout() {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyleAppLayout>
   );
