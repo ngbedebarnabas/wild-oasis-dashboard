d1d3c368fac69ab341696ae7310f6846c212cda1
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-03-19 11:17:26 +0100

    Implementing Dark Mode

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -9,8 +9,8 @@ export function useUpdateUser() {
     mutationFn: updateCurrentUser,
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated.");
-      // queryClient.setQueriesData("user", user);
-      queryClient.invalidateQueries({ queryKey: ["user"] });
+      queryClient.setQueriesData(["user"], user);
+      // queryClient.invalidateQueries({ queryKey: ["user"] });
     },
     onError: (err) => toast.error(err.message),
   });
